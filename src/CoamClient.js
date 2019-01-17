e40230dcc7aa55ee4adb002d7502d0d2318db2dd
Ivan Stanishev <istanishev@cimpress.com>
2019-01-17 22:24:47 +0100

    Add missing comma

--- src/CoamClient.js
+++ src/CoamClient.js
@@ -52,7 +52,7 @@ class CoamClient {
                 retryDelay: (retryCount) => {
                     return this.retryDelayInMs;
                 },
-                shouldResetTimeout: true
+                shouldResetTimeout: true,
             });
         }
     }
