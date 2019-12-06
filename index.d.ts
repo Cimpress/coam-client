2e52571b10c69c43da363368fc5ab40a377432f9
Daan Boerlage <dboerlage@cimpress.com>
2019-12-06 15:14:54 +0100

    Add types for CoamClient

---
+++ index.d.ts
@@ -0,0 +1,129 @@
+type KeyValue<T> = {[key: string]: T};
+
+type Profile = any;
+
+export interface CoamClientOptions {
+  baseUrl?: string;
+  timeout?: number;
+  accessToken?: string;
+  retryAttempts?: number;
+  retryDelayInMs?: number;
+  retryOnForbidden?: boolean;
+  debugFunction?: (message?: any, ...optionalParams: any[]) => void;
+  errorFunction?: (message?: any, ...optionalParams: any[]) => void;
+}
+
+interface GroupPrimitiveArray {
+  groups: GroupPrimitive[];
+}
+
+interface GroupPrimitive {
+  name: string;
+  description: string;
+  id: number;
+  created_by?: string;
+  created_at?: string;
+}
+
+interface Group extends GroupPrimitive {
+  members: GroupMember[];
+  resources: Resource[];
+  permissions: KeyValue<ResourceIdentifierPermissions[]>;
+}
+
+interface GroupMember {
+  principal: string;
+  is_admin: boolean;
+  roles: string[];
+  description?: string;
+}
+
+interface ResourceArray {
+  resources: Resource[];
+}
+
+interface Resource {
+  resource_type: string;
+  resource_identifier: string;
+}
+
+interface ResourceIdentifierPermissions {
+  identifier: string;
+  permissions: string[];
+  resource_type: string;
+}
+
+interface RoleArray {
+  roles: Role[]
+}
+
+interface Role {
+  name: string;
+  description: string;
+  permissions: ResourceTypePermissionDescription[];
+  id: string;
+  created_at?: string;
+}
+
+interface ResourceTypePermissionDescription {
+  resource_type: string;
+  name: string;
+  description: string;
+}
+
+interface CanonicalPrincipalPrimitiveArrayObject {
+  canonical_principals: CanonicalPrincipalPrimitive[];
+}
+
+interface CanonicalPrincipalPrimitive {
+  is_client?: boolean;
+  account_id?: string;
+  canonical_principal: string;
+  is_pending: boolean;
+  profiles: Profile[];
+}
+
+interface Principal {
+  groups: PrincipalGroup[];
+  permissions: KeyValue<ResourceIdentifierPermissions[]>;
+  canonical_principal: string;
+  is_client: boolean;
+  is_pending: boolean;
+  profile?: Profile;
+  account_id?: string;
+}
+
+interface PrincipalGroup {
+  name: string;
+  description: string;
+  id: string;
+  created_by?: string;
+  created_at?: string;
+  is_admin: boolean;
+}
+
+export declare class CoamClient {
+  constructor(options: CoamClientOptions);
+
+  buildGroupUrlFromId(groupId: string): string;
+  hasPermission(principal: string, resourceType: string, resourceIdentifier: string, permission: string): Promise<boolean>;
+  grantRoleToPrincipal(groupUrl: string, principal: string, roleName: string): Promise<void>;
+  getGroupInfo(groupUrl: string): Promise<Group>;
+  setAdminFlag(groupId: string, principal: string, isAdmin: boolean): Promise<void>;
+  removeUserRole(groupId: string, principal: string, role: string): Promise<void>;
+  addUserRole(groupId: string, principal: string, role: string): Promise<void>;
+  modifyUserRoles(groupId: string, principal: string, rolesChanges: string): Promise<void>;
+  addGroupMember(groupId: string, principal: string, isAdmin: boolean): Promise<void>;
+  removeGroupMember(groupId: string, principal: string): Promise<void>;
+  getRoles(): Promise<RoleArray>;
+  findPrincipals(query: string): Promise<CanonicalPrincipalPrimitiveArrayObject>;
+  getPrincipal(principal: string): Promise<Principal>;
+  createGroup(name: string, description: string): Promise<Group>;
+  removeGroup(groupId: string): Promise<void>;
+  findGroups(resourceType: string, resourceIdentifier: string): Promise<GroupPrimitiveArray>;
+  removeResourceFromGroup(groupId: string, resourceType: string, resourceId: string): Promise<ResourceArray>;
+  addResourceToGroup(groupId: string, resourceType: string, resourceId: string): Promise<ResourceArray>;
+  getUserPermissionsForResourceType(principal: string, resourceType: string): Promise<ResourceIdentifierPermissions[]>;
+  getUsersWithPermission(resourceType: string, resourceIdentifier: string, permission: string): Promise<string[]>;
+  createGroupWithUser(principalToCreateGroup: string, principalToAddToGroup: string, groupName: string, groupDescription: string, rolesToAdd: string, resourcesToAdd: string): Promise<string>;
+}
